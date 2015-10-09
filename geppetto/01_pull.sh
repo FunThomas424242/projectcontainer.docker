08b7df71d9972b541ff078da7d24febad409d5d7
Thomas Schubert <funthomas424242@gmail.com>
2015-10-09 23:54:41 +0200

    Sicherheitsabfrage zum Löschen des Containers vor neuem Download eingebaut

--- geppetto/01_pull.sh
+++ geppetto/01_pull.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-docker run --name "geppetto-ide" -it -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix funthomas424242/puppet-geppetto-ide.docker
+#!/bin/bash
+read -t10 -n1 -r -p "!!! Attention the container will be LOST !!! Press any key to continue..." key
+
+if [ "$?" -eq "0" ]; then
+   docker rm geppetto-ide
+   docker run --name "geppetto-ide" -it -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix funthomas424242/puppet-geppetto-ide.docker
+else
+    echo 'No key was pressed.'
+fi
+

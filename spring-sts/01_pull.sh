ad2e60881a140c38002c367237bf842a04a40984
Thomas Schubert <funthomas424242@gmail.com>
2015-10-11 18:58:44 +0200

    spring-sts-ide aufgenommen

---
+++ spring-sts/01_pull.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+read -t10 -n1 -r -p "!!! Attention the container will be LOST !!! Press any key to continue..." key
+
+if [ "$?" -eq "0" ]; then
+   docker rm spring-sts-ide
+   docker run --name "spring-sts-ide" -it -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix funthomas424242/spring-sts-ide.docker
+else
+    echo 'No key was pressed.'
+fi
+
---
+++ spring-sts/attach.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+docker exec -it spring-sts-ide bash
+
+
